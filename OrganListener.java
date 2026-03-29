20413b5de26bb272acf87d9352fc819ba755081e
Store.Infamous <aryan11222567@gmail.com>
2026-03-29 17:57:34 +0530

    Update OrganListener.java

--- OrganListener.java
+++ OrganListener.java
@@ -7,47 +7,41 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-public class    @EventHandler
-    public void onKill(PlayerDeathEvent e) {
+public class OrganListener implements Listener {
+
+    private final OrganSMP plugin;
+
+    public OrganListener(OrganSMP plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+
         Player victim = e.getEntity();
         Player killer = victim.getKiller();
 
         if (killer == null || killer.equals(victim)) return;
 
-        // Anti-Alt System
-        if (killer.getAddress().getAddress().equals(victim.getAddress().getAddress())) {
-            killer.sendMessage(ChatColor.RED + "You cannot steal organs from your own IP!");
-            return;
-        }
+        OrganManager mgr = plugin.getManager();
 
-        String targetOrgan = null;
-        int maxStr = -1;
+        OrganType type = mgr.getStrongest(victim);
+        int amount;
 
-        // Find Strongest Organ
-        for (String type : organTypes) {
-            int str = plugin.getStrength(victim, type);
-            if (str > maxStr) {
-                maxStr = str;
-                targetOrgan = type;
-            }
-        }
-
-        int stolenAmount;
-        if (maxStr <= 0) {
-            // New player victim: Killer gets 10 strength in a random organ
-            targetOrgan = organTypes[plugin.getRandom().nextInt(organTypes.length)];
-            stolenAmount = 10;
+        if (type == null) {
+            type = mgr.getRandom();
+            amount = 10;
         } else {
-            // Victim had power: Killer steals it all
-            stolenAmount = maxStr;
-            plugin.setStrength(victim, targetOrgan, 0);
+            amount = mgr.getStrength(victim, type);
+            mgr.setStrength(victim, type, 0);
         }
 
-        int killerCurrent = plugin.getStrength(killer, targetOrgan);
-        plugin.setStrength(killer, targetOrgan, killerCurrent + stolenAmount);
+        int current = mgr.getStrength(killer, type);
+        mgr.setStrength(killer, type, current + amount);
 
-        killer.sendMessage(ChatColor.GREEN + "✔ You ripped out " + victim.getName() + "'s " + targetOrgan + " (+" + stolenAmount + " STR)");
-        killer.playSound(killer.getLocation(), Sound.ENTITY_ITEM_PICKUP, 1f, 1f);
-        victim.sendMessage(ChatColor.RED + "☠ Your " + targetOrgan + " was stolen by " + killer.getName() + "!");
+        killer.sendMessage(ChatColor.GREEN + "Stole " + amount + " " + type.name);
+        victim.sendMessage(ChatColor.RED + "Lost your " + type.name);
+
+        killer.playSound(killer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
     }
 }
